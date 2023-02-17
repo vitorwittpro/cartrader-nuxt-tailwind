8983827138b0ffb5c8dca1c255cd3c54a986603c
VitorHWitt <vitor.witt@aluno.unc.br>
2023-02-16 23:57:36 -0300

    contact spacing

--- components/Car/Detail/Contact.vue
+++ components/Car/Detail/Contact.vue
@@ -1,24 +1,24 @@
 <template>
   <form class="border-t py-10 w-full">
     <h1 class="text-xl font-bold">Contact The Owner</h1>
-    <div class="w-2/3 gap-5 flex flex-row mt-5">
+    <div class="w-5/6 flex flex-row mt-5">
       <input
-        class="w-full border-2 px-3 py-2 rounded-md text-lg text-gray-600 focus:outline-none"
+        class="w-4/12 border-2 mr-2 px-3 py-2 rounded-md text-md text-gray-600 focus:outline-none"
         type="text"
         placeholder="Name"
       />
       <input
-        class="w-full border-2 px-3 py-2 rounded-md text-lg text-gray-600 focus:outline-none"
+        class="w-5/12 border-2 mr-2 px-3 py-2 rounded-md text-md text-gray-600 focus:outline-none"
         type="text"
         placeholder="Email"
       />
       <input
-        class="w-full border-2 px-3 py-2 rounded-md text-lg text-gray-600 focus:outline-none"
+        class="w-3/12 border-2 px-3 py-2 rounded-md text-md text-gray-600 focus:outline-none"
         type="text"
         placeholder="Phone"
       />
     </div>
-    <div class="w-2/3 mt-5 mb-5">
+    <div class="w-5/6 mt-5 mb-5">
       <textarea
         class="w-full border-2 p-2 text-lg text-gray-600 focus:outline-none rounded-md"
         name="description"
